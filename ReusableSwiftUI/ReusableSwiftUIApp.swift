6ea0081347a2b24abc39fe573b7693ec6739f67a
Babul Raj <babul.raj@moengagae.com>
2023-09-06 13:01:18 +0530

    Initial Commit

---
+++ ReusableSwiftUI/ReusableSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReusableSwiftUIApp.swift
+//  ReusableSwiftUI
+//
+//  Created by Babul Raj on 06/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ReusableSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReusableSwiftUIUITests/ReusableSwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ReusableSwiftUIUITestsLaunchTests.swift
+//  ReusableSwiftUIUITests
+//
+//  Created by Babul Raj on 06/09/23.
+//
+
+import XCTest
+
+final class ReusableSwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
